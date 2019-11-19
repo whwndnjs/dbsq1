d9428f734710161fbfbcf76cf4ad42e22d82891d
jwCho <whwndnjs97@gmail.com>
2019-11-19 12:48:29 +0900

    2019.11.19

---
+++ system.sql
@@ -0,0 +1,18 @@
+SELECT *
+FROM DBA_DATA_FILES;
+
+--table space 생성
+  CREATE TABLESPACE TS_DBSQL
+   DATAFILE 'D:\B_UTIL\4.ORACLE\APP\ORACLE\ORADATA\XE\DBSQL.DBF'
+   SIZE 100M 
+   AUTOEXTEND On;
+   
+   --사용자 추가
+create user whwndnjs identified by java
+default tablespace TS_DBSQL
+temporary tablespace temp
+quota unlimited on TS_DBSQL
+quota 0m on system;
+
+--접속, 생성권한
+GRANT CONNECT, RESOURCE TO whwndnjs;
